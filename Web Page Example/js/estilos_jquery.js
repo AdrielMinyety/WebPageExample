af5d9b93035e84c2a1b03ab1d44f5356558de360
unknown <AM3114779@AM3114779.c3edu.online>
2018-11-28 16:57:31 -0400

    barra de navegacion interactiva

--- Web Page Example/js/estilos_jquery.js
+++ Web Page Example/js/estilos_jquery.js
@@ -1,26 +1,48 @@
 $(document).ready(function(){
 
 // =========MENU MOVIL====================
+var flag = false; 
+var pantalla = $(window).height();
+$("div.hero").pantalla;
 
-$('div.menu-movil').click(menuMovilDesplegable);
-$("i.volver").click(volver);
-// $(window).scroll();
+$(window).scroll(stickyMenu);
+$('i.btn-menu').click(menuMovilDesplegable);
+
+$(window).on("resize", function() {
+	if ($(this).width() > 580) {
+		$("nav.navegacion").show();
+		$("i.btn-menu").addClass("fa-arrow-circle-left");
+		$("i.btn-menu").removeClass("fa-bars");
+	} 
+	else {
+		$("nav.navegacion").hide();
+		$("i.btn-menu").addClass("fa-bars");
+		$("i.btn-menu").removeClass("fa-arrow-circle-left");
+	}
+});
 
 function menuMovilDesplegable(){
-	$("div.menu-movil").toggle("i.volver");
-	$("i.volver").fadeIn(800);	
-	$("nav.nav-movil").slideToggle();
+	$("nav.navegacion").slideToggle();
+	$("i.btn-menu").toggleClass("fa-bars");
+	$("i.btn-menu").toggleClass("fa-arrow-circle-left");
 };
 
-function volver() {
-	$("i.volver").hide();	
-	$("nav.nav-movil").slideToggle();
-	$("div.menu-movil").fadeToggle();
-}
+function stickyMenu() {
+	var scroll = $(window).scrollTop();
 
-// function menuMovil() {
-// 	var scroll = $("div.hero").height();
-// }
+	if (scroll > $("div.hero").height() + 50) {
+		if (!flag) {
+			console.log("FUNCIONA!!!");
+			$("div.barra").css({"position":"fixed", "top":"0px"});
+			flag = true;
+		}
+	} else {
+		if (flag) {
+			$("div.barra").css({"position":"static"});
+			flag = false;
+		}
+	}
+}
 // =====================================
 
 });
