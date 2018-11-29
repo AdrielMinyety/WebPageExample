82f900c7cad03674f504ba689048ac52ada22c2b
unknown <AM3114779@AM3114779.c3edu.online>
2018-11-29 00:02:21 -0400

    animacion mejorada, pero con problemas de optimizacion

--- Web Page Example/js/estilos_jquery.js
+++ Web Page Example/js/estilos_jquery.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 // =========MENU MOVIL====================
+var prevScrollPos = window.pageYOffset;
 var flag = false; 
 var pantalla = $(window).height();
 $("div.hero").pantalla;
@@ -9,6 +10,7 @@ $(window).scroll(stickyMenu);
 $('i.btn-menu').click(menuMovilDesplegable);
 
 $(window).on("resize", function() {
+	// Responsive
 	if ($(this).width() > 580) {
 		$("nav.navegacion").show();
 		$("i.btn-menu").addClass("fa-arrow-circle-left");
@@ -31,6 +33,7 @@ function stickyMenu() {
 	var scroll = $(window).scrollTop();
 
 	if (scroll > $("div.hero").height() + 50) {
+		hideShowMenu();
 		if (!flag) {
 			console.log("FUNCIONA!!!");
 			$("div.barra").css({"position":"fixed", "top":"0px"});
@@ -42,6 +45,19 @@ function stickyMenu() {
 			flag = false;
 		}
 	}
+}
+
+function hideShowMenu() {
+	var currentScrollPos = window.pageYOffset;
+	if (prevScrollPos > currentScrollPos) {
+		$("div.barra").css({"top":"0px"});
+		console.log("sadas");
+	} else {
+		$("div.barra").css({"top":"-400px"});
+		console.log("sadas2");
+	}
+
+	prevScrollPos = currentScrollPos;
 }
 // =====================================
 
