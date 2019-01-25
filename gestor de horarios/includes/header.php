4b719f995a3ec9b8fda22b4eef56cee4a4bb024b
unknown <AM3114779@AM3114779.c3edu.online>
2019-01-25 18:15:43 -0400

    Nuevo repositorio y archivos ordenados

---
+++ gestor de horarios/includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Agenda</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+	<!-- FONT AWESOME -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+	
+</head>
+<body>
+
+	<nav class="navbar navbar-dark bg-dark">
+		<div class="container">
+			<a href="index.php" class="navbar-brand font-weight-bold">Agenda <i class="fas fa-check-double"></i></a>
+		</div>
+	</nav>
